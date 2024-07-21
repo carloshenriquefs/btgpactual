eccf869c40f7786cbf33944cb3aa619e8437a9fc
Carlos Henrique <ichfs9108@gmail.com>
2024-07-21 02:39:41 -0300

    feat: Implementar testes - OrderService

--- orderms/src/test/java/tech/buildrun/btgpactual/orderms/factory/OrderCreatedEventFactory.java
+++ orderms/src/test/java/tech/buildrun/btgpactual/orderms/factory/OrderCreatedEventFactory.java
@@ -8,10 +8,20 @@ import java.util.List;
 
 public class OrderCreatedEventFactory {
 
-    public static OrderCreatedEvent build() {
+    public static OrderCreatedEvent buildWithOneItem() {
 
         var itens = new OrderItemEvent("notebook", 1, BigDecimal.valueOf(20.50));
         var event = new OrderCreatedEvent(1L, 2L, List.of(itens));
+
+        return event;
+    }
+
+    public static OrderCreatedEvent buildWithTwoItem() {
+
+        var item1 = new OrderItemEvent("notebook", 1, BigDecimal.valueOf(20.50));
+        var item2 = new OrderItemEvent("mouse", 1, BigDecimal.valueOf(35.25));
+
+        var event = new OrderCreatedEvent(1L, 2L, List.of(item1, item2));
 
         return event;
     }
